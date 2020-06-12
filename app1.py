4508ad81921d6b6624f78e3b96845120c9f0cff2
jukomol <komolma37@gmail.com>
2020-06-12 13:21:04 +0600

    refresh button added
    you can refresh data after finding  a  word

--- app1.py
+++ app1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from tkinter import *
 import json
 from difflib import get_close_matches
@@ -37,51 +36,27 @@ def outputresult():
         else:
             t1.insert(END, output)
 
+
+def clearText():
+    t1.config(state=NORMAL)
+    t1.delete(1.0,END)
+    e1.delete(0,'end')
+
+
 e1_value = StringVar()
 e1=Entry(window, textvariable= e1_value)
 e1.grid(row=0, column=0)
 
 b1=Button(window, text="Find", command= outputresult)
-b1.grid(row=0, column=1)
+b1.grid(row=1, column=0)
 
 t1= Text(window, height=3, width= 60)
-t1.grid(row=1, column=0)
+t1.grid(row=2, column=0)
+
+b2=Button(window, text="Refresh", command= clearText)
+b2.grid(row=1,column=1)
 
 
 
 
 window.mainloop()
-=======
-import json
-from difflib import get_close_matches
-
-data = json.load(open("data.json"))
-
-def translate(w):
-    w = w.lower()
-    if w in data:
-        return data[w]
-    elif w.title() in data:  # if user entered "texas" this will check for "Texas" as well.
-        return data[w.title()]
-    elif w.upper() in data: #in case user enters words like USA or NATO
-        return data[w.upper()]
-    elif len(get_close_matches(w, data.keys())) > 0:
-        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
-        yn=yn.upper()
-        if yn == "Y":
-            return data[get_close_matches(w, data.keys())[0]]
-        elif yn == "N":
-            return "The word doesn't exist. Please double check it."
-        else:
-            return "We didn't understand your entry"
-    else:
-        return "The word doesn't exist. Please double check it."
-
-word = input("Enter word: ")
-output = translate(word)
-if type(output) == list:
-    for item in output:
-        print(item)
-else:
-    print(output)
->>>>>>> d55ec21e165ac8df6154cd0f150a75ebeabc0fdd
